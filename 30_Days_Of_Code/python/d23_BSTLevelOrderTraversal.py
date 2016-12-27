2ecffe0585863c35159e95aee3b92025be19bb52
Runcy Oommen <runcy.oommen@gmail.com>
2016-12-28 01:05:49 +0530

    Days 23 and 24

---
+++ 30_Days_Of_Code/python/d23_BSTLevelOrderTraversal.py
@@ -0,0 +1,35 @@
+# import sys
+
+class Node:
+    def __init__(self, data):
+        self.right = self.left = None
+        self.data = data
+
+class Solution:
+    def insert(self, root, data):
+        if root is None:
+            return Node(data)
+        else:
+            if data <= root.data:
+                cur = self.insert(root.left, data)
+                root.left = cur
+            else:
+                cur = self.insert(root.right, data)
+                root.right = cur
+        return root
+
+    def levelOrder(self, root):
+        # Write your code here
+        d = [root]
+        while any(d):
+            print(*map(lambda n: n.data, d), sep=' ', end=' ')
+            c = sum([[a.left, a.right] for a in d], [])
+            d = list(filter(bool, c))
+
+T = int(input())
+myTree = Solution()
+root = None
+for i in range(T):
+    data = int(input())
+    root = myTree.insert(root, data)
+myTree.levelOrder(root)
