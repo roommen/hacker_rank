77e1aea35e0262143e1072737aa1b0c1622ae88b
Runcy Oommen <runcy.oommen@gmail.com>
2017-03-23 13:30:01 +0530

    Complete height of a binary tree

---
+++ data_structures/trees/python/tree_height_of_a_binary_tree.py
@@ -0,0 +1,67 @@
+class Node:
+    def __init__(self, info):
+        self.info = info
+        self.left = None
+        self.right = None
+        self.level = None
+
+    def __str__(self):
+        return str(self.info)
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def create(self, val):
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+'''
+class Node:
+      def __init__(self,info):
+          self.info = info
+          self.left = None
+          self.right = None
+
+
+       // this is a node of the tree , which contains info as data, left , right
+'''
+
+def height(root):
+    heightCount = 0
+    if root.left:
+        heightCount = 1 + height(root.left)
+    if root.right:
+        heightCount = 1 + height(root.right)
+    return heightCount
+
+
+tree = BinarySearchTree()
+t = int(input())
+
+for _ in range(t):
+    x = int(input())
+    tree.create(x)
+
+print(height(tree.root))
