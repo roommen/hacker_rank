794551e4834d206954f6a6cc220d702e3497ce9b
Runcy Oommen <runcy.oommen@gmail.com>
2017-06-14 14:46:01 +0530

    Submit improvement (partially solved) to WoC33 - Day 1(Twin arrays) challenge

--- week_of_code_33/twin_arrays.py
+++ week_of_code_33/twin_arrays.py
@@ -3,38 +3,30 @@ n = int(input())
 A = [int(x) for x in input().split(' ')]
 B = [int(x) for x in input().split(' ')]
 
-A_copy, B_copy = A, B
-# print(A_copy, B_copy)
-minA, minB = min(A_copy), min(B_copy)
-# print(minA, minB)
-global_min = 999999999
-try:
-    while A.index(minA) == B.index(minB):
-        if minA > minB:
-            A_copy.pop(A.index(minA))
-        elif minB > minA:
-            B_copy.pop(B.index(minB))
-        else:
-            A_copy.pop(A.index(minA))
-            B_copy.pop(B.index(minB))
+i, min_, sum_ = 0, 99999999, 0
+minA, minB = min(A), min(B)
+if A.index(minA) != B.index(minB):
+    print(minA+minB)
+    # sortA = sorted(A)
+    # print(sortA)
+else:
+    # sortA = sorted(A)
+    # sortB = sorted(B)
+    # i = 0
+    # while A.index(sortA[i]) == B.index(sortB[i]):
+    #     sortA.pop(i)
+    #     sortB.pop(i)
+    #     i += 1
+    # print(sortA[i-1] + sortB[i-1])
 
-        new_min = minA + minB
-        # print(new_min)
-        if new_min < global_min:
-            global_min = new_min
+    while i < n:
+        j = 0
+        while j < n:
+            if i != j:
+                sum_ = A[i] + B[j]
+                if sum_ < min_:
+                    min_ = sum_
+            j += 1
+        i += 1
 
-        minA, minB = min(A_copy), min(B_copy)
-    else:
-        new_min = minA + minB
-        # print(new_min)
-        if new_min < global_min:
-            global_min = new_min
-
-    print(global_min)
-except ValueError:
-    # print(global_min)
-    new_min = minA + minB
-    if new_min < global_min:
-        global_min = new_min
-
-    print(global_min)
+    print(min_)
