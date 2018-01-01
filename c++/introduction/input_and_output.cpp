6a1c1650de6d1a54b3698590e075e8ca5a3c1b99
Runcy Oommen <runcy.oommen@gmail.com>
2018-01-01 23:26:21 +0530

    Beginning with c++ this new year. Get serious about it

---
+++ c++/introduction/input_and_output.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv) {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    cout << a+b+c;
+
+    return 0;
+}
---
+++ c++/introduction/say_hello_world!_with_C++.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv) {
+    cout << "Hello, World!";
+
+    return 0;
+}
