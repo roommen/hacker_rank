fe8b05e0e73425df5d4011b290add418d461eef9
Runcy Oommen <runcy.oommen@gmail.com>
2018-05-28 23:12:09 +0530

    Solve operator overloading

---
+++ c++/other_concepts/overload_operators.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+
+using namespace std;
+
+class Complex
+{
+public:
+    int a,b;
+    void input(string s)
+    {
+        int v1=0;
+        int i=0;
+        while(s[i]!='+')
+        {
+            v1=v1*10+s[i]-'0';
+            i++;
+        }
+        while(s[i]==' ' || s[i]=='+'||s[i]=='i')
+        {
+            i++;
+        }
+        int v2=0;
+        while(i<s.length())
+        {
+            v2=v2*10+s[i]-'0';
+            i++;
+        }
+        a=v1;
+        b=v2;
+    }
+};
+
+//Overload operators + and << for the class complex
+//+ should add two complex numbers as (a+ib) + (c+id) = (a+c) + i(b+d)
+//<< should print a complex number in the format "a+ib"
+
+ostream& operator<<(ostream& os, const Complex& c) {
+    // return os << c.a << (c.b > 0 ? '+' : '-') << 'i' << c.b;
+    return os << c.a << '+i' << c.b;
+}
+
+Complex operator+(const Complex& a, const Complex& b) { 
+    return { a.a + b.a, a.b + b.b };
+}
+
+
+
+
+
+
+int main()
+{
+    Complex x,y;
+    string s1,s2;
+    cin>>s1;
+    cin>>s2;
+    x.input(s1);
+    y.input(s2);
+    Complex z=x+y;
+    cout<<z<<endl;
+}
+
+
+
+
+
